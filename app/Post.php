460b67384a5a17766b1b490e13c4004931f4a894
Alex <gurievalex78@gmail.com>
2020-10-21 15:48:43 +0200

    model

---
+++ app/Post.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'title', 
+        'body', 
+        'user_id',
+        'slug',
+        'updated_at'
+    ];
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+}
