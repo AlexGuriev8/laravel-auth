2bb1ecd30099f3fa3d883ef75b4daba4dc5b2144
Alex <gurievalex78@gmail.com>
2020-10-22 15:06:43 +0200

    title

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,4 +1,13 @@
 @extends('layouts.app')
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
 
 @section('content')
 <form action="{{route('posts.store')}}" method="POST">
@@ -8,7 +17,7 @@
   <div class="form-row">
     <div class="col-md-6 mb-3">
       <label for="title">Titolo</label>
-      <input type="text" placeholder="Titolo" class="form-control"  name="title" value="{{old('titolo')}}">
+      <input type="text" placeholder="Titolo" class="form-control"  name="title" value="{{old('title')}}">
     </div>
   </div>
   <div class="form-row">
@@ -17,7 +26,16 @@
       <textarea placeholder="Descrizione" class="form-control" name="body"  cols="30" rows="10">{{old('body')}}</textarea>
     </div>
   </div>
+  <div class="form-row">
+     <div class="col-md-6 mb-3">
+        @foreach ($tags as $tag)
+           <label for="tag">{{$tag->name}}</label> 
+     <input type="checkbox" name="tags[]" value="{{$tag->id}}">
+        @endforeach
+     </div>
+  </div>
   <button class="btn btn-primary" type="submit">Create</button>
 </form>
     </div>
 @endsection
+
